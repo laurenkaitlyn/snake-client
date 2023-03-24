69c1de062178ae23ca32b36c02ac4ada4f163922
Lauren <laurenkaitlyn@gmail.com>
2023-03-24 13:31:12 -0400

    refactoring

--- client.js
+++ client.js
@@ -20,4 +20,4 @@ const connect = function (data) {
   })
 };
 
-module.exports = connect;
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,27 +1,7 @@
-const { stdin } = require('process');
-const connect = require('./client');
+const { connect } = require('./client');
+const { setInput } = require('./input');
 
 console.log("Connecting ...");
 connect();
-
-//set up interface to handle user input from stdin
-const handleUserInput = function () {
-  process.stdin.on('data', (key) => {
-    // exit if user presses ctrl + c
-    if (key === '\u0003') {
-      process.exit();
-    }
-  })
-
-}
-
-const setInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
 
 setInput();
