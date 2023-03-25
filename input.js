8e55b36f10762dd86e7408d754245aead6c6f0da
Lauren <laurenkaitlyn@gmail.com>
2023-03-25 15:21:24 -0400

    cleaned up movement keys

--- input.js
+++ input.js
@@ -3,15 +3,14 @@ const { stdin } = require('process');
 let connection;
 
 //set up interface to handle user input from stdin
-const handleUserInput = function () {
-  stdin.on('data', (key) => {
+const handleUserInput = function (key) {
     // exit if user presses ctrl + c
     if (key === '\u0003') {
       process.exit();
     }
-  })
+  }
 
-}
+
 
 const setInput = function (conn) {
   connection = conn;
@@ -21,48 +20,33 @@ const setInput = function (conn) {
   stdin.resume();
   stdin.on("data", handleUserInput);
 
-    //move up
+    
   stdin.on('data', (key) => {
+    //move up
     if (key === 'w') {
       conn.write("Move: up");
     }
-  });
-
-  //move right
-  stdin.on('data', (key) => {
+     //move right
     if (key === 'd') {
       conn.write("Move: right");
     }
-  });
-
-  //move down
-  stdin.on('data', (key) => {
+    //move down
     if (key === 's') {
       conn.write("Move: down");
     }
-  });
-
-  //move left
-  stdin.on('data', (key) => {
+    //move left
     if (key === 'a') {
       conn.write("Move: left");
     }
-  });
-
-  //say #1
-  stdin.on('data', (key) => {
+    //say #1
     if (key === 'v') {
       conn.write("Say: sup");
     }
-  });
-
-  //say #2
-  stdin.on('data', (key) => {
+    //say #2
     if (key === 'f') {
       conn.write("Say: chonky snek");
     }
   });
-
 
   return stdin;
 };
